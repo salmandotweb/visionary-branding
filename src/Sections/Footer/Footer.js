635e739ebf633fd68885eef8082bc0cc63842885
Muhammad Salman <salmandotweb@gmail.com>
2022-03-22 03:02:40 +0500

    visionary-branding

--- src/Sections/Footer/Footer.js
+++ src/Sections/Footer/Footer.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { BsFacebook, BsLinkedin } from "react-icons/bs";
+import { AiFillInstagram } from "react-icons/ai";
 import classes from "./Footer.module.css";
 
 const Footer = () => {
@@ -13,6 +15,18 @@ const Footer = () => {
       <p className={classes.copyright}>
         &copy; Copyright {year} Visionary Branding. All right reserved
       </p>
+      <div className={classes.footer_right}>
+        <div className={classes.links}>
+          <p>Privacy Policy</p>
+          <p>Terms</p>
+          <p>Get in Touch</p>
+        </div>
+        <div className={classes.social_links}>
+          <BsFacebook />
+          <BsLinkedin />
+          <AiFillInstagram />
+        </div>
+      </div>
     </footer>
   );
 };
