f57ea8abaa7b759e77f3a8c502a068f7589a6ecd
Muhammad Salman <salmandotweb@gmail.com>
2022-03-24 03:56:51 +0500

    visionary-branding

--- src/Components/Loader/Loader.js
+++ src/Components/Loader/Loader.js
@@ -4,7 +4,7 @@ import classes from "./Loader.module.css";
 const Loader = () => {
   return (
     <div className={classes.loading_wrapper}>
-      <div class={`${classes.loader} ${classes.loader2}`}></div>
+      <div className={`${classes.loader} ${classes.loader2}`}></div>
     </div>
   );
 };
